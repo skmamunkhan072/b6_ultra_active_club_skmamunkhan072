75827baf8ad02add1e7ec1bd9218100032925a83
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-09-29 07:49:00 +0600

    data img set

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,15 +3,19 @@ import "./Card.css";
 const Card = ({ data }) => {
   console.log(data);
   const { id, name, details, img, time, quantity, email } = data;
+  console.log(img);
+  const newDetails = details.slice(0, 100);
+  console.log(newDetails);
   return (
     <div>
       <div className="card card-compact bg-base-100 shadow-xl">
         <figure className="card_img">
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          <img src={img} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">{name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{newDetails}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
