d671c56e4497e2e151d975b92744cba25d6d6003
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-09-29 06:56:57 +0600

    data fach add

--- src/components/WebBody/WebBody.js
+++ src/components/WebBody/WebBody.js
@@ -4,6 +4,13 @@ import RightSiteBarContainer from "../RightSiteBarContainer/RightSiteBarContaine
 import "./WebBody.css";
 
 const WebBody = () => {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    fetch(`Fackdb.json`)
+      .then((res) => res.json())
+      .then((datas) => setDatas(datas));
+  }, []);
+  console.log(datas);
   return (
     <div className="web_body grid">
       <CardWraperContainer></CardWraperContainer>
