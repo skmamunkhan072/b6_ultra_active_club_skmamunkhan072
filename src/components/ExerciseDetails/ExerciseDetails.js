9d1d65792c81876bf1c511a530d2ceff81807613
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-09-30 14:46:33 +0600

    toast Successful add

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -1,8 +1,19 @@
 import "./ExerciseDetails.css";
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ExerciseDetails = ({exercisSeconds , requiredTime}) => {
 
-
+  const notify = () => toast.success('🦄 Successful your message Wow so easy!!!', {
+    theme:"dark",
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
   return (
     <div>
       <h1 className="my-6 text-xl">Exercise Details</h1>
@@ -18,7 +29,8 @@ const ExerciseDetails = ({exercisSeconds , requiredTime}) => {
       </div>
 
       <div className="mt-10 text-center">
-        <button className="bg-teal-600 hover:bg-teal-400 text-black  py-2 px-5 rounded-md font-semibold">Activity Completed</button>
+        <button onClick={notify} className="bg-teal-600 hover:bg-teal-400 text-black  py-2 px-5 rounded-md font-semibold">Activity Completed</button>
+        <ToastContainer/>
       </div>
     </div>
   );
